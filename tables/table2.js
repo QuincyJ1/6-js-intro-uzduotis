3b6f03430cf58bca0e8d89537162df2c00460f1c
Dainius <dainiusnarkevicius1@gmail.com>
2023-05-31 17:30:16 +0300

    Update

--- tables/table2.js
+++ tables/table2.js
@@ -3,16 +3,16 @@ import multiplier from '../numbers/multiplier.js';
 import { n1, n2, n3, n4, n5, n6, n7, n8, n9, n10 } from '../numbers/numbers.js';
 
 
-const table2 = `${multiplier} * ${n1} = ${multiplier * skaicius}
+const table2 = `${multiplier} * ${n1} = ${multiplier * n1}
 ${multiplier} * ${n2} = ${multiplier * n2}
 ${multiplier} * ${n3} = ${multiplier * n3}
-${multiplier} * ${n4} = ${multiplier *n4  }
-${multiplier} * ${n5} = ${multiplier * }
-${multiplier} * ${n6} = ${multiplier * }
-${multiplier} * ${n7} = ${multiplier * }
-${multiplier} * ${n8} = ${multiplier * }
-${multiplier} * ${n9} = ${multiplier * }
-${multiplier} * ${n10} = ${multiplier * }
+${multiplier} * ${n4} = ${multiplier * n4}
+${multiplier} * ${n5} = ${multiplier * n5}
+${multiplier} * ${n6} = ${multiplier * n6}
+${multiplier} * ${n7} = ${multiplier * n7}
+${multiplier} * ${n8} = ${multiplier * n8}
+${multiplier} * ${n9} = ${multiplier * n9}
+${multiplier} * ${n10} = ${multiplier * n10}
 `
 
 
